afb1357f803f5d5f4f96e92366e939737d7878d9
Muna Chimoriya <muna.chimoriya.2022student.lhschools.org>
2020-04-02 19:23:30 -0400

    Conditional Implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// kjen;kewjndlke
-        efwefwefef
-              efwefewf
-        wefwefewfwefew
+//      if(true){
+//       // if conditional is true then execute all code in between this code block
+//      }
+        //else if (different conditional){
+        //if previous conditional are all false and this is true then the code in between this code will execute
+        // }
+
+        //else {
+        // if the conditional is false then execute all the code between this code
+        // }
+
+        int numberJ = 13;
+        int numberK = 8;
+
+        if (numberJ == 20) {
+            System.out.println("The number is equal to 20");
+
+        }
+        else if(numberJ >= 20 ){
+            System.out.println( "The number is equal or larger than 20 ");
+        }
+        else{
+            System.out.println("The number is smaller than 20");
+        }
     }
 }
